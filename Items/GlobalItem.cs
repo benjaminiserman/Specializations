471364829ab799716a042cda80135a260b838b67
winggar <b@1051productions.com>
2018-03-05 23:28:03 -0600

    Icon
    and other little things

---
+++ Items/GlobalItem.cs
@@ -0,0 +1,23 @@
+﻿/*using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Specializations.Items
+{
+    public class ThrowingAmmo : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == ItemID.Shuriken)
+            {
+                item.ammo = AmmoID.Bullet;
+            }
+            if (item.type == ItemID.RottenEgg)
+            {
+                item.ammo = ItemID.Shuriken;
+            }
+
+        }
+    }
+}
+*/
---
+++ Items/Throwing/CobaltShuriken.cs
@@ -0,0 +1,34 @@
+﻿using Specializations.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Specializations.Items.Throwing
+{
+	public class CobaltShuriken : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 10f;
+			item.damage = 45;
+			item.knockBack = 5f;
+			item.useStyle = 1;
+			item.useAnimation = 25;
+			item.useTime = 25;
+			item.width = 30;
+			item.height = 30;
+			item.maxStack = 999;
+			item.rare = 5;
+
+			item.consumable = true;
+			item.noUseGraphic = true;
+			item.noMelee = true;
+			item.autoReuse = true;
+			item.thrown = true;
+
+			item.UseSound = SoundID.Item1;
+			item.value = Item.sellPrice(silver: 5);
+			//item.shoot = mod.ProjectileType<CobaltShuriken>();
+		}
+	}
+}
